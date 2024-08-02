ed97ff1ee31715f824de1a73d5cdce5f2c812073
Jake1152 <rancho974@gmail.com>
2024-08-02 22:52:01 +0900

    feat: DB table column type decorator

--- src/main/entities/main-movie-view.entity.ts
+++ src/main/entities/main-movie-view.entity.ts
@@ -1,28 +1,36 @@
-import { ViewEntity, ViewColumn } from 'typeorm';
+import { ViewEntity, ViewColumn, Column } from 'typeorm';
 
 @ViewEntity({ name: 'main_movie_view' })
 export class MainMovieView {
   @ViewColumn()
+  @Column({ type: 'int' })
   movie_id: number;
 
   @ViewColumn()
+  @Column({ type: 'character varying', length: 150 })
   movie_title: string;
 
   @ViewColumn()
+  @Column({ type: 'numeric' })
   before_price: number;
 
   @ViewColumn()
+  @Column({ type: 'numeric' })
   after_price: number;
 
   @ViewColumn()
+  @Column({ type: 'date' })
   before_date: Date;
 
   @ViewColumn()
+  @Column({ type: 'date' })
   after_date: Date;
 
   @ViewColumn()
+  @Column({ type: 'text' })
   movie_poster: string;
 
   @ViewColumn()
+  @Column({ type: 'char', length: 3 })
   country: string;
 }
