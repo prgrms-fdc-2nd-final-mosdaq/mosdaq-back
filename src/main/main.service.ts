c6f055b5eb0a4f700a539aa69be9872327acfee8
Jake1152 <rancho974@gmail.com>
2024-08-08 12:56:45 +0900

    feat: 개봉한 지 4~24주, 투표 마감된 영화 인기순 5개
    - view로 미리 제약조건을 준 상태
    - 개월수를 고정해놓음
    - query builder 결과가 의도와 다른 부분 발생, 카멜케이스가 안되거나
      특정 값이 안나오거나 등등
    - 숫자가 문자열로 나오는 문제 0 -> '0', DTO는
      숫자라서 != '0'같은 로직 넣는 것 불가.
    - 그로인해 결과 처리 될때는 '0'같은 값이 null로 처리됨
      되는 이슈 발생
    - 추후 수정 필요

--- src/main/main.service.ts
+++ src/main/main.service.ts
@@ -71,8 +71,6 @@ export class MainService {
       // TODO: queryBuilder.getRawMany() 이후 movietitle 처럼 camelCase로 안나오는 이슈 해결
       const movieList = await queryBuilder.getRawMany();
 
-      console.log('# Query Result : ', movieList);
-
       return {
         movieList: movieList.map((movie) => ({
           movieId: parseInt(movie.movieid, 10),
@@ -104,74 +102,36 @@ export class MainService {
     }
   }
 
+  // TODO: 투표 수가 0일때 '0'으로 가져와지고 그로인하여 결과가 null로 나오는 이슈 해결 필요
   async getPopularMoviesPolled(): Promise<PopularMoviesPolledResponseDto> {
     try {
-      const POPULAR_MOVIE_POLLED_COUNT = 5;
+      const POPULAR_MOVIE_POLLED_COUNT = 15;
 
-      const queryBuilder = this.popularMoviePolledRepository
-        .createQueryBuilder('p')
-        .select([
-          'p.movieId AS movieId',
-          'p.moviePoster AS moviePoster',
-          'p.movieTitle AS movieTitle',
-          'COALESCE(p.upPolls, 0) AS upPolls',
-          'COALESCE(p.downPolls, 0) AS downPolls',
-          'COALESCE(p.pollCount, 0) AS pollCount',
-          'p.country AS country',
-          'p.beforePrice AS beforePrice',
-          'p.afterPrice AS afterPrice',
-          'p.beforeDate AS beforeDate',
-          'p.afterDate AS afterDate',
-        ])
-        .orderBy('p.movieOpenDate', 'DESC')
-        .limit(POPULAR_MOVIE_POLLED_COUNT);
+      const queryBuilder: SelectQueryBuilder<PopularMoviePolledView> =
+        this.popularMoviePolledRepository
+          .createQueryBuilder('p')
+          .orderBy('p.movieOpenDate', 'DESC')
+          .limit(POPULAR_MOVIE_POLLED_COUNT);
 
-      // const queryBuilder = this.popularMoviePolledRepository
-      //   .createQueryBuilder('p')
-      //   .select([
-      //     'p.movieId',
-      //     'p.moviePoster',
-      //     'p.movieTitle',
-      //     'COALESCE(p.upPolls, 0) AS upPolls',
-      //     'COALESCE(p.downPolls, 0) AS downPolls',
-      //     'COALESCE(p.pollCount, 0) AS pollCount',
-      //     'p.country AS country',
-      //     'p.beforePrice',
-      //     'p.afterPrice',
-      //     'p.beforeDate',
-      //     'p.afterDate',
-      //   ])
-      //   .orderBy('p.movieOpenDate', 'DESC')
-      //   .limit(POPULAR_MOVIE_POLLED_COUNT);
-
-      // const queryBuilder: SelectQueryBuilder<PopularMoviePolledView> =
-      //   this.popularMoviePolledRepository
-      //     .createQueryBuilder('p')
-      //     .orderBy('p.movieOpenDate', 'DESC')
-      //     .limit(POPULAR_MOVIE_POLLED_COUNT);
-
-      // const movies = await queryBuilder.getRawMany();
       const movies = await queryBuilder.getMany();
 
-      console.log('Query Result : ', movies);
-
       const movieList = movies.map((movie) => ({
-        movieId: Number(movie.movieid),
-        posterUrl: String(movie.movieposter),
-        movieTitle: String(movie.movietitle),
+        movieId: Number(movie.movieId),
+        posterUrl: String(movie.moviePoster),
+        movieTitle: String(movie.movieTitle),
         up:
-          movie.pollcount && movie.uppolls !== null
-            ? Math.round(Number((movie.uppolls / movie.pollcount) * 100))
+          movie.pollCount && movie.upPolls !== null
+            ? Math.round(Number((movie.upPolls / movie.pollCount) * 100))
             : 0,
         down:
-          movie.pollcount && movie.downpolls !== null
-            ? Math.round(Number((movie.downpolls / movie.pollcount) * 100))
+          movie.pollCount && movie.downPolls !== null
+            ? Math.round(Number((movie.downPolls / movie.pollCount) * 100))
             : 0,
         countryCode: String(movie.country),
-        beforePrice: Number(movie.beforeprice),
-        afterPrice: Number(movie.afterprice),
-        beforePriceDate: format(new Date(movie.beforedate), 'yyyy-MM-dd'),
-        afterPriceDate: format(new Date(movie.afterdate), 'yyyy-MM-dd'),
+        beforePrice: Number(movie.beforePrice),
+        afterPrice: Number(movie.afterPrice),
+        beforePriceDate: format(new Date(movie.beforeDate), 'yyyy-MM-dd'),
+        afterPriceDate: format(new Date(movie.afterDate), 'yyyy-MM-dd'),
       }));
 
       return { movieList, movieListCount: movieList.length };
@@ -183,52 +143,4 @@ export class MainService {
       );
     }
   }
-
-  /*
-  async getPopularMoviesPolled(): Promise<PopularMoviesPolledResponseDto> {
-    try {
-      // 투표 마감 영화 검색 최대 갯수
-      const POPULAR_MOVIE_POLLED_COUNT = 5;
-
-      const queryBuilder: SelectQueryBuilder<PopularMoviePolledView> =
-        this.popularMoviePolledRepository
-          .createQueryBuilder('p')
-          .orderBy('p.movieOpenDate', 'DESC')
-          .limit(POPULAR_MOVIE_POLLED_COUNT);
-
-      const movies = await queryBuilder.getMany();
-
-      console.log('Query Reulst : ', movies);
-
-      // BUG, TODO: up, down, price 등은 number 타입인데 왜 json 응답 값에서는 string 타입인가?
-      const movieList = movies.map((movie) => ({
-        movieId: Number(movie.movieId),
-        posterUrl: String(movie.moviePoster),
-        movieTitle: String(movie.movieTitle),
-        up:
-          movie.pollCount && movie.upPolls !== null
-            ? Number(((movie.upPolls / movie.pollCount) * 100).toFixed(2))
-            : 0,
-        down:
-          movie.pollCount && movie.downPolls !== null
-            ? Number(((movie.downPolls / movie.pollCount) * 100).toFixed(2))
-            : 0,
-
-        countryCode: String(movie.country.trim()),
-        beforePrice: Number(movie.beforePrice),
-        afterPrice: Number(movie.afterPrice),
-        beforePriceDate: new Date(movie.beforeDate).toISOString(),
-        afterPriceDate: new Date(movie.afterDate).toISOString(),
-      }));
-
-      return { movieList: movieList, movieListCount: movieList.length };
-    } catch (err) {
-      console.error('Error fetching popular polled movies:', err);
-      throw new HttpException(
-        '투표 중인 영화 목록을 가져오는데 실패 하였습니다.',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-  }
-  */
 }
