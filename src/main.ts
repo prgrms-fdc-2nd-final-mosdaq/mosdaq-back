b00e03440bd49940c16ff44fa1b80bdbb1cdb73c
Jake1152 <rancho974@gmail.com>
2024-08-08 16:22:34 +0900

    fix: origin 조건 로직 수정

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,17 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   // TODO: 프론트도 AWS 배포 이후에는 cors 조건 한정시키는 것으로 변경!
-  const allowedOrigins = [''];
+  const allowedOrigins = [];
 
   app.enableCors({
     origin: (origin, callback) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin); // 요청 도메인을 Access-Control-Allow-Origin 헤더에 설정
       } else {
         callback(new Error('Not allowed by CORS'));
       }
     },
-    credentials: true,
+    credentials: true, // 자격 증명 허용
   });
 
   setupSwagger(app);
