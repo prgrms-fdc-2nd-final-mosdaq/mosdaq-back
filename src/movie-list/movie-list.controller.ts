336ed202a17643200dfeb4ac78c6c769a8a51724
Jake1152 <rancho974@gmail.com>
2024-08-13 00:55:47 +0900

    feat: movie-list controller
    - controller를 통한 간단한 문자열 응답 값 반환

--- src/movie-list/movie-list.controller.ts
+++ src/movie-list/movie-list.controller.ts
@@ -1,28 +1,68 @@
-import { Controller, Get, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Query, Req } from '@nestjs/common';
 import { MovieListService } from './movie-list.service';
+import {
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 // import { CreateMovieListDto } from './dto/create-movie-list.dto';
 
-@Controller('movie-list')
+@Controller('api/v1/movie/list')
+@ApiTags('영화 투표 목록 api')
 export class MovieListController {
   constructor(private readonly movieListService: MovieListService) {}
 
-  // @Post()
-  // create(@Body() createMovieListDto: CreateMovieListDto) {
-  //   return this.movieListService.create(createMovieListDto);
-  // }
-
-  @Get()
-  findAll() {
-    return this.movieListService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.movieListService.findOne(+id);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.movieListService.remove(+id);
+  // /api/v1/movie/list?poll=true&offset={}&limit={}&sort={}
+  @Get('/')
+  @ApiOperation({
+    summary: '투표 중인 영화 목록 API',
+    description:
+      '투표 중인 영화 목록을 제공한다.\
+      \n 페이지네이션과 개봉일 기준 오름차순, 내림차순 정렬 등의 조건을 \
+      query param을 통해 설정 할 수 있다.',
+  })
+  // TODO: schema는 별도 파일로 모듈화
+  @ApiOkResponse({
+    description: '투표 중인 영화 목록을 제공한다.',
+    schema: {
+      type: 'object',
+      properties: {
+        movieList: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              movieId: { type: 'number' },
+              movieTitle: { type: 'string' },
+              posterUrl: { type: 'string' },
+              up: { type: 'number' },
+              down: { type: 'number' },
+              afterPrice: { type: 'number' },
+              beforePriceDate: { type: 'string', format: 'date' },
+              afterPriceDate: { type: 'string', format: 'date' },
+            },
+          },
+        },
+        movieListCount: { type: 'number' },
+      },
+    },
+  })
+  @ApiInternalServerErrorResponse({
+    description: '서버 내부 오류로 인해 영화 목록을 가져올 수 없습니다.',
+  })
+  pollMovieList(@Query('poll') poll: string, @Req() req: unknown) {
+    try {
+      if (poll === 'true') {
+        return '/api/v1/movie/list?poll=true';
+      } else if (poll === 'false') {
+        return '/api/v1/movie/list?poll=false';
+      } else {
+        throw new Error('Invalid poll query parameter');
+      }
+    } catch (err) {
+      console.error('Error in /api/v1/movie/list?poll=true  : ', poll);
+      throw err;
+    }
   }
 }
