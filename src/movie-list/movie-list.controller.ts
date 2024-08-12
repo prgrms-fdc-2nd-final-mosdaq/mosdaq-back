1f04d5c7cf48a6090a75768cb5bd217e68934505
Jake1152 <rancho974@gmail.com>
2024-08-13 01:37:01 +0900

    docs: movie-list, 투표 중인 영화 목록 swagger 수정

--- src/movie-list/movie-list.controller.ts
+++ src/movie-list/movie-list.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Query, Req } from '@nestjs/common';
 import { MovieListService } from './movie-list.service';
 import {
+  ApiBadRequestResponse,
   ApiInternalServerErrorResponse,
   ApiOkResponse,
   ApiOperation,
+  ApiQuery,
   ApiTags,
 } from '@nestjs/swagger';
-// import { CreateMovieListDto } from './dto/create-movie-list.dto';
+import { PollingMovieListDto } from './dto/polling-movie-list-response.dto';
 
 @Controller('api/v1/movie/list')
 @ApiTags('영화 투표 목록 api')
@@ -15,6 +17,18 @@ export class MovieListController {
 
   // /api/v1/movie/list?poll=true&offset={}&limit={}&sort={}
   @Get('/')
+  @ApiQuery({
+    name: 'poll',
+    required: false,
+    description: '투표 결과 필터링 여부',
+  })
+  @ApiQuery({ name: 'offset', required: false, description: '결과의 오프셋' })
+  @ApiQuery({ name: 'limit', required: false, description: '결과의 제한 수' })
+  @ApiQuery({
+    name: 'sort',
+    required: false,
+    description: '정렬 순서 (ASC 또는 DESC)',
+  })
   @ApiOperation({
     summary: '투표 중인 영화 목록 API',
     description:
@@ -25,37 +39,40 @@ export class MovieListController {
   // TODO: schema는 별도 파일로 모듈화
   @ApiOkResponse({
     description: '투표 중인 영화 목록을 제공한다.',
-    schema: {
-      type: 'object',
-      properties: {
-        movieList: {
-          type: 'array',
-          items: {
-            type: 'object',
-            properties: {
-              movieId: { type: 'number' },
-              movieTitle: { type: 'string' },
-              posterUrl: { type: 'string' },
-              up: { type: 'number' },
-              down: { type: 'number' },
-              afterPrice: { type: 'number' },
-              beforePriceDate: { type: 'string', format: 'date' },
-              afterPriceDate: { type: 'string', format: 'date' },
-            },
-          },
+    type: PollingMovieListDto,
+    isArray: true,
+  })
+  @ApiBadRequestResponse({
+    description:
+      '쿼리 파라미터 형식을 맞추어주세요.\
+      \n\n e.g) "/api/v1/movie/list?poll=true&offset=5&limit=1&sort=ASC"',
+    content: {
+      'application/json': {
+        example: {
+          message: '잘못된 요청입니다.',
         },
-        movieListCount: { type: 'number' },
       },
     },
   })
   @ApiInternalServerErrorResponse({
     description: '서버 내부 오류로 인해 영화 목록을 가져올 수 없습니다.',
+    content: {
+      'application/json': {
+        example: {
+          message: '서버 내부 오류',
+        },
+      },
+    },
   })
+  // TODO: response type 명시
   async pollMovieList(@Query('poll') poll: string, @Req() req: unknown) {
     try {
       if (poll === 'true') {
+        // TODO: query 파라미터 검증, 데이터 뽑아내기
+        // TODO: pagination, sorting
         return this.movieListService.getPollingMovies();
       } else if (poll === 'false') {
+        // TODO: 투표 마감된 영화 목록
         return '/api/v1/movie/list?poll=false';
       } else {
         throw new Error('Invalid poll query parameter');
