6c446b973cabd519513fb5bb665d4c69e3595968
shgusgh12 <https://github.com/shgusgh12>
2024-08-14 18:49:34 +0900

    feat: optional guard 구현

---
+++ src/auth/optionalAccessToken.guard.ts
@@ -0,0 +1,43 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+
+@Injectable()
+export class OptionalAccessTokenGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    // 토큰이 없는 경우, 인증 없이 통과
+    if (!token) {
+      return true;
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get<string>('JWT_SECRET'),
+      });
+      request['user'] = payload; // 유효한 토큰일 경우 사용자 정보를 request에 추가
+    } catch (error) {
+      throw new UnauthorizedException('유효하지 않은 토큰입니다.'); // 토큰이 유효하지 않은 경우 예외 발생
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
