b644e3370ebbbbb7730e2a34e3889995d112cf5f
Jake1152 <rancho974@gmail.com>
2024-08-14 15:47:00 +0900

    remove: movie-list pipe 파일 삭제, 추후 구현 시 추가

--- src/movie-list/movie-list.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MovieListPipe } from './movie-list.pipe';
-
-describe('MovieListPipe', () => {
-  it('should be defined', () => {
-    expect(new MovieListPipe()).toBeDefined();
-  });
-});
