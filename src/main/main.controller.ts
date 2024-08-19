903f5d522cac6375cf7afa6f1ee2b99545b2ef7c
Jake1152 <rancho974@gmail.com>
2024-08-19 14:29:21 +0900

    docs: main controller swagger 응답 방식 수정
    - @ApiResponse => @ApiOkResponse, @ApiInternalServerErrorResponse
      - 400, 500 error

--- src/main/main.controller.ts
+++ src/main/main.controller.ts
@@ -1,6 +1,13 @@
 import { Controller, Get, Query, Req } from '@nestjs/common';
 import { MainService } from './main.service';
-import { ApiTags, ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiQuery,
+  ApiOkResponse,
+  ApiInternalServerErrorResponse,
+} from '@nestjs/swagger';
+import { SWAGGER_INTERNAL_SERVER_ERROR_CONTENT } from 'src/constants';
 
 @Controller('api/v1/main-movie')
 @ApiTags('대표 영화 api')
@@ -13,8 +20,7 @@ export class MainController {
     description:
       '대표 영화 5개의 영화 데이터 및 개봉 4주 전, 4주 후 주가 데이터 요청',
   })
-  @ApiResponse({
-    status: 200,
+  @ApiOkResponse({
     description:
       '서비스 운영자가 선정한 영화 5개의 영화 데이터 및 개봉 4주 전, 4주 후 주가 데이터와 함께 반환합니다.',
     schema: {
@@ -40,9 +46,9 @@ export class MainController {
       },
     },
   })
-  @ApiResponse({
-    status: 500,
+  @ApiInternalServerErrorResponse({
     description: '서버 내부 오류로 인해 영화 목록을 가져올 수 없습니다.',
+    content: SWAGGER_INTERNAL_SERVER_ERROR_CONTENT,
   })
   async mainMovie() {
     // TOOD: 에러 핸들링
@@ -61,8 +67,7 @@ export class MainController {
     type: 'string',
     description: '투표 활성화 여부 (true)',
   })
-  @ApiResponse({
-    status: 200,
+  @ApiOkResponse({
     description: '성공적으로 투표 중인 영화 목록을 반환했습니다.',
     schema: {
       type: 'object',
@@ -86,9 +91,9 @@ export class MainController {
       },
     },
   })
-  @ApiResponse({
-    status: 500,
+  @ApiInternalServerErrorResponse({
     description: '서버 내부 오류로 인해 영화 목록을 가져올 수 없습니다.',
+    content: SWAGGER_INTERNAL_SERVER_ERROR_CONTENT,
   })
   // TODO: @Req() req: any => @Req() req: Request  req.user 데이터 타입을 모르는 이슈 발생
   async popularPollingMovies(@Query('poll') poll: string, @Req() req: any) {
